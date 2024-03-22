32f2a14596074c643e4b29d37a6c2a1565c47700
Roger Tan <teochewthunder@gmail.com>
2024-03-22 23:49:09 +0800

    Update liverpool-bar-chart.py

--- liverpool-bar-chart.py
+++ liverpool-bar-chart.py
@@ -1,6 +1,25 @@
 import numpy as np
 import matplotlib.pyplot as plt 
- 
+
+def barChart(labels, vals):
+    fig = plt.figure(figsize = (10, 5))
+
+    # creating the bar plot
+    plt.bar(labels, vals, color ='maroon', 
+            width = 0.8)
+
+    for index, value in enumerate(vals):
+        plt.text(index - (len(str(value)) * 0.02), value + 1, str(value))
+
+    plt.ylim(0, max(stats) + 5)
+
+    plt.xlabel("Players")
+    plt.ylabel("No. of goals scored")
+    plt.title("Liverpool FC Player Stats")
+    plt.show()
+    
+def seasonName(year):
+    return str(year) + "/" + str(year + 1)
   
 # creating the dataset
 data = {
@@ -42,25 +61,36 @@ data = {
     }
 }
 
-players = list(data[1998].keys())
-values = list(data[1998].values())
-
-stats = [];
-for v in values:
-    stats.append(v['goals'])
-  
-fig = plt.figure(figsize = (10, 5))
+seasons = list(data.keys())
     
-# creating the bar plot
-plt.bar(players, stats, color ='maroon', 
-        width = 0.4)
+ans = ""
 
-for index, value in enumerate(stats):
-    plt.text(index, value + 1, str(value))
+while (ans != "0"):
+    print ("Select a season")
     
-plt.ylim(0, max(stats) + 5)
- 
-plt.xlabel("Players")
-plt.ylabel("No. of goals scored")
-plt.title("Liverpool FC Player Stats")
-plt.show()
+    for i, s in enumerate(seasons):
+        print (str(i + 1) + ": " + seasonName(s))
+        
+    print ("0: Exit")
+    
+    input(ans)
+    season = seasons[ans - 1]
+    
+    print ("Select a stat")
+    print ("1: goals")
+    print ("2: appearances")
+    print ("0: Exit")
+
+    input(ans)
+    if (ans == "1"): stat = "goals"
+    if (ans == "2"): stat = "appearances"
+    
+    players = list(data[season].keys())
+    values = list(data[season].values())
+
+    stats = [];
+    for v in values:
+        stats.append(v[stat])
+    
+
+    barChart(players, stats)
